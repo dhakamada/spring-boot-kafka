29d4b648ee6662dbc62e6364afdaa09a13862d9d
dhakamada <dhakamada@uolinc.com>
2018-01-15 13:40:58 -0200

    Initializing kafka project.

---
+++ src/main/java/br/com/dhakamada/kafka/Application.java
@@ -0,0 +1,18 @@
+package br.com.dhakamada.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dhakamada
+ * @version $Revision: $<br/>
+ * $Id: $
+ * @since 1/15/18 1:31 PM
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
